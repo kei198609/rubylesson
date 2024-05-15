96d56e11cdb8aec33c890ebb3ec7392de838e4eb
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-05-16 08:09:39 +0900

    change javascript lesson for main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
+{
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
 
+  btn.addEventListener('click', () => {
+    const results = ['大吉','中吉','小吉']
+    const n =Math.floor(Math.random() * results.length);
+    result.textContent = results[n];
+  });
+}
+
+
+// 問題: 与えられた文字列 s を逆転させます。
+
+// 例:
+// 入力: s = "hello"
+// 出力: "olleh"
+  function reverseString(s) {
+    return s.sprit('').reverse().join('');
+  }
