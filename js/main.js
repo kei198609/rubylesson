fdfeddbcb59ce9393cba252014f98b132e473b8c
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-05-17 09:11:00 +0900

    change javascript lesson for main.js

--- js/main.js
+++ js/main.js
@@ -20,3 +20,33 @@
   function reverseString(s) {
     return s.sprit('').reverse().join('');
   }
+
+
+//   問題: 文字列 s 内の特定の文字 char の出現回数をカウントします。
+
+// 例:
+// 入力: s = "hello", char = "l"
+// 出力: 2
+
+  function count(s, char) {
+    return s.sprit(char).length - 1;
+  }
+
+  // 問題: 二つの文字列 s1 と s2 がアナグラムかどうかを判断します。
+
+  // 例:
+  // 入力: s1 = "listen", s2 = "silent"
+  // 出力: true
+
+  function anagram(s1, s2) {
+    const sortStr = (str) => {
+      return str.sprit('').sort().join('');
+    };
+    return sortStr(s1) === sortStr(s2);
+  }
+
+  // 省略記法(アロー関数の中括弧 {} と return キーワードを省略)
+  function anagram(s1, s2) {
+    const sortStr = (str) => str.sprit('').sort().join('');
+    return sortStr(s1) === sortStr(s2);
+  }
