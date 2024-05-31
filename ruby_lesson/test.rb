e461efb4632c968e60c7c878bb1c305b6f219faf
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-06-01 06:59:11 +0900

    change ruby lesson for test.rb

--- ruby_lesson/test.rb
+++ ruby_lesson/test.rb
@@ -105,9 +105,21 @@ a & b #=>[3]
 'Ruby'.chars #=>["R", "u", "b", "y"]
 'Ruby,Java,Python'#=>["Ruby", "Java", "Python"]
 
+# Arrayを使った配列作成
+a = Array.new(5)
+a #=>[nil, nil, nil, nil, nil]
 
+a = Array,new(5, 0)
+a #=>[0, 0, 0, 0, 0]
 
-# mapメソッド
+# *で配列同士を非破壊的に連結する
+a = [1, 2, 3]
+[a] #=>[[1, 2, 3]]
+[*a] #=>[1, 2, 3]
+[-1, 0, *a, 4, 5] #=>[-1,0,1,2,3,4,5]
+
+# *********mapメソッド*********
+
 # mapメソッドを使わない場合
 numbers = [1,2,3,4,5]
 new_numbers = []
@@ -120,3 +132,98 @@ new_numbers = []
 new_numbers = numbers.map {|n| n * 10}
 new_numbers #=> [10,20,30,40,50]
 
+
+
+# *********ブロックについて詳しく*********
+fruits = ['apple', 'orange', 'melon']
+fruits.each_with_index { |fruit, i| puts "#{i}:#{fruit}" }
+# 0:apple
+# 1:orange
+# 2:melon
+
+# 名前に"a"を含み、なおかつ添え字が奇数の要素を削除
+fruits.delete_if.with_index{ |fruit, i| fruit.include?('a') && i.add? }
+# ["apple", "melon"]
+
+# ブロックの後ろに別のメソッドを続けて書くこともできる
+names = ['田中', '鈴木', '佐藤']
+names.map {|name| "#{name}さん"}.join('と') #=> 田中さんと鈴木さんと佐藤さん
+# {}　とdo endは同じなので以下のようにendのあとにjoinを追記できるけど、{}で書く方が読みやすい
+name.map do |name|
+    "#{name}さん"
+end.join('と')
+
+
+
+# *********ブロックを利用するメソッドの定義とyield*********
+# 例としてまずはこんなメソッドを用意
+def greet
+    puts 'おはよう'
+    puts 'こんばんは'
+end
+# メソッドを実行するとこうなる
+greet
+#=>
+# おはよう
+# こんばんは
+
+# ブロック付きでメソッドを呼び出す
+greet do
+    puts 'こんにちは'
+end
+# エラーは起きないが出力結果が変わることはない
+#=>
+# おはよう
+# こんばんは
+
+# なので、渡されたブロックを実行するためには、メソッド内でyieldを使う。
+def greet
+    puts 'おはよう'
+    yield
+    puts 'こんばんは'
+end
+#=>
+# おはよう
+# こんにちは
+# こんばんは
+
+
+# yieldはブロックに引数を渡したり、ブロックの戻り値を受け取ったりもできる。
+# 以下はこんにちはという引数をブロックに渡し、
+# ブロックの戻り値を受け取ってputsメソッドで出力するコード例
+def greet
+    puts 'おはよう'
+    text = yield 'こんにちは'
+    puts text
+    puts 'こんばんは'
+end
+
+greet do |text|
+    text * 2
+end
+#=>
+# おはよう
+# こんにちはこんにちは
+# こんばんは
+
+
+# ブロックを引数として明示的に受け取ることもできる。
+# ブロックを引数として受け取る場合は、引数名の前に&をつける。
+# そのブロックを実行する場合は、callメソッドを使う。
+# def メソッド(引数)
+#     引数.call
+# end
+
+def greet(&hoge)
+    puts 'おはよう'
+    text = hoge.call('こんにちは')
+    puts text
+    puts 'こんばんは'
+end
+greet do |text|
+    text * 2
+end
+#=>
+# おはよう
+# こんにちはこんにちは
+# こんばんは
