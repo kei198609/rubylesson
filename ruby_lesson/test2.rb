9a039589ac5fdca0f31a73cdf2c9155ac2cd8227
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-06-02 06:56:52 +0900

    change ruby lesson for test2.rb

--- ruby_lesson/test2.rb
+++ ruby_lesson/test2.rb
@@ -398,3 +398,22 @@ def single_number(nums)
     nums.each { |num| single ^= num }
     single
 end
+
+# ********9. Palindrome Number********
+# Example 1:
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# Example 2:
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+def is_palindrome(x)
+    string = x.to_s
+    string == string.reverse
+end
