ca4feee25fb23b31be4f0f93ec69579a0d7e27e5
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-07-14 06:55:16 +0900

    change ruby lesson for test2.rb

--- ruby_lesson/test2.rb
+++ ruby_lesson/test2.rb
@@ -4099,3 +4099,103 @@ end
 
 player = Hello.new()
 player.say_hello("ruby")
+
+
+
+
+# ********RPGのPlayerクラスを継承で記述するその2********
+# RPGの攻撃シーンを表示するプログラムで、teamのメンバーが順番にattackメソッドを呼び出します。
+# 下記の期待する出力値が出力されるように、右のコードの足りない部分を補ってください。
+# 期待する出力値
+# 勇者はスライムを攻撃した
+# 魔法使いはスライムを攻撃した
+# 戦士はスライムを猛攻撃した！
+class Player
+    def initialize(name)
+        @name = name
+    end
+
+    def attack(enemy)
+        puts  "#{@name}は#{enemy}を攻撃した"
+    end
+end
+
+class Warrior < Player
+    def attack(enemy)
+        puts  "#{@name}は#{enemy}を猛攻撃した！"
+    end
+end
+
+
+team = []
+team.push(Player.new("勇者"))
+team.push(Player.new("魔法使い"))
+team.push(Warrior.new("戦士"))
+
+team.each do |person|
+    person.attack("スライム")
+end
+
+
+
+# ********クラスの中のメソッドを呼び出す********
+# Greetingクラスの中に、say_helloメソッドとsay_yeahメソッドを定義しています。
+# say_helloメソッドを呼び出すと「hello paiza」と表示されて、
+# say_yeahメソッドを呼び出すと「YEAH YEAH YEAH!」と表示されます。
+
+# 「hello paiza」に続けて、「YEAH YEAH YEAH!」と表示されるようコードを書き換えてください。
+# 期待する出力値
+# hello paiza
+# YEAH YEAH YEAH!
+class Greeting
+    def initialize()
+        @msg = "hello"
+        @target = "paiza"
+    end
+    def say_hello()
+        puts "#{@msg} #{@target}"
+        say_yeah() #ここを追記
+    end
+
+    private
+    def say_yeah()
+        puts "YEAH YEAH YEAH!"
+    end
+end
+
+player = Greeting.new()
+player.say_hello()
+
+# ********親クラスのメソッドを呼び出す********
+# Greetingクラスでsay_helloメソッドを定義して、Helloクラスでsay_helloメソッドをオーバーライドしています。
+
+# Helloクラスでsay_helloメソッドを書き換えて、「hello paiza」に続けて、「YEAH YEAH YEAH!」と表示されるようにください。
+# 期待する出力値
+# hello paiza
+# YEAH YEAH YEAH!
+
+class Greeting
+    def initialize()
+        @msg = "hello"
+        @target = "paiza"
+    end
+
+    def say_hello()
+        puts "#{@msg} #{@target}"
+    end
+end
+
+class Hello < Greeting
+    def say_hello()
+        super #ここを追記
+        puts "YEAH YEAH YEAH!"
+    end
+end
+
+player = Hello.new()
+player.say_hello()
+
+# オーバーライドされたメソッドがあった場合、
+# スーパークラスのではなくサブクラスのメソッドの方が呼び出されますが、
+# オーバーライドされたメソッドの中で super と記述すると、
+# スーパークラスの同じ名前のメソッドを呼び出すことができます。
