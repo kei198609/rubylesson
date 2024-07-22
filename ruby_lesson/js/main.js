5b0dca077e36da52432e52530cdd7ad348c7a1dd
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-07-23 06:26:35 +0900

    add javascript lesson for main.js

---
+++ ruby_lesson/js/main.js
@@ -0,0 +1,63 @@
+// ********コードで値が取得 ********
+// readline モジュールのセットアップ
+// readline モジュールを使用して、標準入力 (process.stdin) と
+// 標準出力 (process.stdout) を扱うインターフェースを作成します。
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// line イベントのリスナーを設定
+// readline インターフェースは、標準入力から一行ずつ読み取るたびに line イベントを発火します。
+// このイベントが発生するたびに、コールバック関数が呼び出され、その行のデータが引数 line として渡されます。
+// このコールバック関数では、受け取った行のデータを lines 配列に追加しています。
+reader.on('line', (line) => {
+  // 文字列 s を取得
+  lines.push(line);
+});
+
+// close イベントのリスナーを設定
+// 入力ストリームが終了すると、readline インターフェースは close イベントを発火します。
+// このイベントが発生すると、コールバック関数が呼び出され、lines 配列からデータを取り出して出力します。
+// 配列には、標準入力から読み取った各行が格納されています。
+reader.on('close', () => {
+  // 文字列 s をそのまま出力し、末尾に改行を追加
+  console.log(lines[0]);
+});
+
+
+// ********半角スペース区切りの 2 つの入力 ********
+// 入力される値
+// 以下の形式で標準入力によって与えられます。
+// 半角スペースで区切られた文字列 s_1, s_2 が 1 行で与えられます。
+
+
+// s_1 s_2
+
+// 入力値最終行の末尾に改行が１つ入ります。
+// 文字列は標準入力から渡されます。
+
+// 期待する出力
+// 入力された文字列 s_1, s_2 をそれぞれ改行区切りで出力してください。
+// また、末尾に改行を入れ、余計な文字、空行を含んではいけません。
+
+
+// s_1
+// s_2
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  const [s1, s2] = line.split(' ');
+
+  console.log(s1);
+  console.log(s2);
+});
