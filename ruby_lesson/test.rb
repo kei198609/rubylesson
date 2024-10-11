56dc1876dabbaa45a5b68d15cb9645003ce088b2
kei198609 <keep_casting.19860910@hotmail.co.jp>
2024-10-12 06:47:19 +0900

    add ruby lesson for test.rb

--- ruby_lesson/test.rb
+++ ruby_lesson/test.rb
@@ -1445,5 +1445,29 @@ product.price_tag #=> 1000円
 
 
 
+# モジュールをextendする
+# extendを使うとモジュール内のメソッドをそのクラスの特異メソッド（つまりクラスメソッド）にすることができます。
+module Loggable
+    def log(text)
+        puts "[LOG] #{text}"
+    end
+end
+
+class Product
+    extend Loggable
+
+    def self.create_products(names)
+        # logメソッドをクラスメソッド内で呼び出す（つまりlogメソッド自体もクラスメソッドになっている）
+        log 'create_products is called'
+    end
+end
+
+# クラスメソッド経由でlogメソッドが呼び出される
+Product.create_products([]) #=> [LOG] create_products is called
+# Productクラスのクラスメソッドとして直接呼び出すことも可能
+Product.log('Hello') #=> [LOG] Hello
+
+
+
 
 
