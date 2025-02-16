4e3b1185beb17f9a2298212f746dfd3970950562
kei198609 <keep_casting.19860910@hotmail.co.jp>
2025-02-16 19:40:54 +0900

    add ruby lesson for test.rb

--- ruby_lesson/test.rb
+++ ruby_lesson/test.rb
@@ -6133,3 +6133,41 @@ point(3, 10) #=> x=3, y=10
 
 
 
+# ?で終わるメソッド
+
+# Rubyのメソッド名は?や!で終わらせることができます。
+# ?で終わるメソッドは慣習として真偽値を返すメソッドになっています。
+
+# 空文字列であればtrue,そうでなければfalse
+''.empty? #=> true
+'abc'.empty? #=> false
+
+# 引数の文字列が含まれていればtrue, そうでなければfalse
+'watch'.include?('at') #=> true
+'watch'.include?('in') #=> false
+
+# 奇数ならtrue,偶数ならfalse
+1.odd? #=> true
+2.odd? #=> false
+
+# 偶数ならtrue,奇数ならfalse
+1.even? #=> false
+2.even? #=> true
+
+# オブジェクトがnilであればtrue,そうでなければfalse
+nil.nil? #=> true
+'abc'.nil? #=> false
+
+# ?で終わるメソッドは自分で定義することもできます。
+# 真偽値を返す目的のメソッドであれば、?で終わらせるようにしたほうが良いでしょう。
+
+# 3の倍数ならtrue,それ以外はfalseを返す
+def multiple_of_three?(n)
+    n % 3 == 0
+end
+multiple_of_three?(4) #=> false
+multiple_of_three?(6) #=> true
+
+
+
+
