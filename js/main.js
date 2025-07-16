45141e52e174f69e821c1f99f1396e49a819066a
kei198609 <keep_casting.19860910@hotmail.co.jp>
2025-07-16 19:42:19 +0900

    add javascript question

--- js/main.js
+++ js/main.js
@@ -454,3 +454,26 @@ reader.on('close', () => {
     console.log("No");
   }
 });
+
+
+
+
+// 正の整数 N が与えられます。
+// 1 ~ N の整数を 1 から順に改行区切りで出力してください。
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+const lines = [];
+const reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    const N = Number(lines[0]); // ← 数値に変換
+  for (let i = 1; i <= N; i++) {
+      console.log(i);
+  }
+});
