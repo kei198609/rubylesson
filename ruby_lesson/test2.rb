9d336988614b11509fb504c0940f4f782bff343a
kei198609 <keep_casting.19860910@hotmail.co.jp>
2025-08-26 19:35:32 +0900

    add ruby question

--- ruby_lesson/test2.rb
+++ ruby_lesson/test2.rb
@@ -6758,3 +6758,24 @@ k = gets.to_i
 array = [3,1,4,1,5,9,2,6,5,3]
 puts array[k]
 
+# 繰り返し構文を活用して、次の 10 個の整数の総和を計算し出力するプログラムを作成してください。
+# 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+
+arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+sum = 0
+arr.each do |i|
+    sum += i
+end
+puts sum
+
+
+
+
+# N 個の正整数が与えられます。
+# その中から 3 の倍数をすべて、入力順に改行区切りで出力してください。
+n = gets.to_i
+arr = gets.split.map(&:to_i)
+
+arr.each do |x|
+    puts x if x % 3 == 0
+end
