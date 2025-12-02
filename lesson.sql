948405e4207ae64a098b51809455d0ca45f887a1
kei198609 <keep_casting.19860910@hotmail.co.jp>
2025-12-02 19:36:43 +0900

    add lesson.sql

--- lesson.sql
+++ lesson.sql
@@ -8,3 +8,8 @@ exit
 source /home/ubuntu/sakila-schema.sql
 source /home/ubuntu/sakila-data.sql
 show tables;
+
+-- 都道府県の情報が格納されている、 prefectures テーブルのみをもつデータベースが与えられます。
+-- prefectures テーブルに含まれる、人口が 1,000,000 人未満 の
+-- 都道府県の情報を全て表示するクエリを作成してください。
+SELECT * FROM prefectures WHERE population <= 1000000;
