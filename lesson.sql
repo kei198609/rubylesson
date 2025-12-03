72ddb288586fe6c8de26009cbb47ec9b870e1f32
kei198609 <keep_casting.19860910@hotmail.co.jp>
2025-12-03 19:38:34 +0900

    add lesson.sql

--- lesson.sql
+++ lesson.sql
@@ -13,3 +13,10 @@ show tables;
 -- prefectures テーブルに含まれる、人口が 1,000,000 人未満 の
 -- 都道府県の情報を全て表示するクエリを作成してください。
 SELECT * FROM prefectures WHERE population <= 1000000;
+
+-- 都道府県の情報が格納されている、 prefectures テーブルのみをもつデータベースが与えられます。
+-- prefectures テーブルに含まれるデータを、
+-- 面積が大きい順に並び替えたときの上位 10 件を出力するクエリを作成してください。
+SELECT * FROM prefectures
+ORDER BY area DESC --テーブルのデータを並び替えるには ORDER BY 句を使います。
+LIMIT 10;
